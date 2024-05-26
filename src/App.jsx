aba4857a52c0753d164a59e146039f2c0ad9abb1
prabubalakrishna <prabusingapore89@gmail.com>
2024-05-26 17:13:14 +0530

    array  list and usage keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-
 const App = () => {
-  const friends = [
-    {id: 1, name:'John', age: 20},
-    {id: 2, name:'Peter', age: 30},
+
+
+  const name = 'Peter';	  const friends = [
+    { id: 1, name: 'John', age: 20 },
+    { id: 2, name: 'Peter', age: 30 },
+    { id: 3, name: 'Mark', age: 40}
   ]
-  return (
-    <div>
-      <h1>Friends</h1>
-      <ul>
-        {
-          friends.map(friend index) =>
-            <li key={friend.id}>{friend.name}{friend.age}</li>
+
+
+  return (	  return (
+    <div>	    <div>
+      <Hello 	      <h1>Friends</h1>
+        name={name}	      <ul>
+      />	        {
+          friends.map((friend, index) => 
+            <li key={index}>{friend.name} {friend.age}</li>
           )
         }
       </ul>
-      </div>
-  )
-}
-
-export default App;
+    </div>	    </div>
+  )	  )
+}	}
