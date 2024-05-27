776db811413fac005a89917e30ff06a6005149d4
prabubalakrishna <prabusingapore89@gmail.com>
2024-05-27 10:53:50 +0530

    component

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-React+Vite
+# React- Day -5: React Hooks
 
-This is a React + Vite projet created for the batch FSD59WD-T to demonstrate the concepts of React.
+# Contents:
+
+[ ] What is the hook?
+
+hooks are functions that let you "hookinto" React state and lifecycle features from functio components.
+Hooks don't work isnside classes - they let you use react without classes,
+examples: useState, useEffect, useContext, useReducer, useReg
+
+[] Lifecycle of Hook
+
+-intitialization
+-Mounting
+-update
+-Unmounting
+
+[] useState, useEffect
+
+[] Props drilling
+[]Passing data from child to parent component
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello, World</h1>
-    </div>
-  )
+import React, {Component} from'react';
+
+export class App extends Component {
+
+constructor(props):
+super(props);
+this.state = {
+  count: 0
 }
+}
+//component did Mount
+componentDidMount(){
+  console.log('Component did mount');
+}
+//componentDidupdate
+componentDidUpdate() {
+  console.log('Component did update');
+}
+//componentwillUnmount
+componentWillUnount(){console.log('Component will unmount');
+}
+handleIncrement =()=>{
+  this.setState({
+    count: this.state.count+1
+  })
+}
+return (
+  <div>
+    <h1>Counder:{this.state.count}</h1>
+    <button onClick={this.handleIncrement}></button>
+  </div>
+)
 export default App;
